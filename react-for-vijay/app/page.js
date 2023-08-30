945ce47a04e0f653cb9f37d8976984a78dcdc2b8
Vijay Misal <misalvijay153@gmail.com>
2023-08-30 09:27:00 +0530

    ss

--- react-for-vijay/app/page.js
+++ react-for-vijay/app/page.js
@@ -1,14 +1,31 @@
 'use client'
 import React, { useState } from 'react';
+import axios from 'axios';
 const page = () => {
-  const [marks, setMarks] = useState(80);
-
+  const [Images, setImages] = useState([]);
+  const getImages = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list")
+    try {
+      const data = response.data;
+      setImages(data)
+    } catch (error) {
+      console.error("Error fetching image!")
+    }
+  }
   return (
     <>
-      <h1>
-        my marks were {marks}
-      </h1>
-      <button onClick={() => { setMarks(34) }}> Update</button>
+  
+      <div>
+        <button onClick={getImages} className='px-5 py-3 bg-green-600 text-white'>Get Images</button>
+        <div className='p-10'>
+          {Images.map((elem, i) => {
+            return <img key={i} src={elem.download_url}
+              width={300}
+              height={300}
+              className='m-10 rounded inline-block'
+            />
+          })}
+        </div></div>
     </>
   )
 }
